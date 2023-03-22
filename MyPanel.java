54f902f44a11d2e9cb9f906d866195e98942f8b0
Michelle L <mlawson@smith.edu>
2023-03-21 23:07:37 -0400

    task complete

--- MyPanel.java
+++ MyPanel.java
@@ -2,22 +2,36 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MyPanel extends JPanel{
-
-    Image image;
     
     MyPanel(){
 
-        image = new ImageIcon("woman.png").getImage();
         this.setPreferredSize(new Dimension(500, 500));
     }
 
-    public void paint(Graphics g){//invoked automatically when JFrame is instantiated
-        
-        Graphics2D g2D = (Graphics2D) g;//cast g to Graphics2D
+    //The following paint function draws elements to form a face
+    public void paint(Graphics g){
+        Graphics2D g2D = (Graphics2D) g;
 
+        //face circle
+        g2D.setColor(Color.pink);
+        g2D.fillOval(100, 100, 300, 300);
+
+        // eye whites
+        g2D.setColor(Color.white); 
+        g2D.fillOval(150, 150, 50, 50);
+        g2D.fillOval(300, 150, 50, 50);
+
+        // eye pupils
+        g2D.setColor(Color.black);
+        g2D.fillOval(160, 160, 30, 30);
+        g2D.fillOval(310, 160, 30, 30);
+
+        // mouth
+        g2D.setColor(Color.red);
         g2D.setStroke(new BasicStroke(5));
-
-        g2D.setFont(new Font("Ink Free", Font.BOLD, 20));
-
+        g2D.drawArc(150, 250, 200, 100, 180, 180);
+        
     }
+
+
 }
